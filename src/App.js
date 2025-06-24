1cc7901ee6b789b111867ad112c7a5cf06b6aa5a
MaxL <maximilian.luther2000@gmail.com>
2025-06-24 18:52:33 +0200

    Working (mostly) client UI, no data layer or other major logic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
+import React, { Children, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as UiE from './UiElements.js'
 
 function App() {
+  // New Quest Popup
+  const [isPopupOpen, setIsNewQPopupOpen] = useState(false);
+  const openNewQPopup = () => setIsNewQPopupOpen(true);
+  const closeNewQPopup = () => setIsNewQPopupOpen(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {/* Popup for New Quest */}
+      {isPopupOpen && <UiE.NewQuestPopup isOpen={isPopupOpen} onClose={closeNewQPopup} />}
+
+      {/* left-toolbar-section */}
+      <div className="sidebar">
+        <div className='listing-vertical-stretch' style={{ gap: 30 }}>
+          <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'inline-flex' }}>
+            <img src={logo} alt="Logo" />
+            <div className="font-heading-1">SideQuest</div>
+          </div>
+          <div className='listing-vertical-stretch' style={{ gap: 15 }}>
+            <UiE.IconTextButtonFill text="New Main Quest" id_="AddMainBtn" onClick={openNewQPopup} />
+            <UiE.SimpleDivider />
+            <UiE.BasicLable text="Recents" />
+            <div className="listing-vertical-stretch" style={{ gap: 10 }}>
+              <UiE.IconTextButtonOutline text="QuestType" id_="QuestTypeBtn1" onClick={NotImpl} />
+              <UiE.IconTextButtonOutline text="QuestType" id_="QuestTypeBtn2" onClick={NotImpl}/>
+            </div>
+            <UiE.SimpleDivider />
+            <UiE.BasicLable text="Common" />
+            <div className="listing-vertical-stretch" style={{ gap: 10 }}>
+              <UiE.IconTextButtonOutline text="QuestType" id_="QuestTypeBtn3" onClick={NotImpl}/>
+              <UiE.IconTextButtonOutline text="QuestType" id_="QuestTypeBtn4" onClick={NotImpl}/>
+              <UiE.IconTextButtonOutline text="QuestType" id_="QuestTypeBtn5" onClick={NotImpl}/>
+            </div>
+          </div>
+        </div>
+        <div className='listing-vertical-stretch' style={{ gap: 15 }}>
+          <UiE.SimpleDivider />
+          <UiE.SidebarOption text="Settings" icon="settings" id_="SettingsBtn" onClick={NotImpl}/>
+          <UiE.SidebarOption text="Account" icon="account_circle" id_="AccountBtn" onClick={NotImpl}/>
+        </div>
+      </div>
+
+      {/* center-list-section */}
+      <div className='center-elem'>
+        <div className='font-heading-2'>Your Quests:</div>
+        <QuestList />
+      </div>
+
+      {/* right-info-section */}
+      <div className="sidebar" style={{ overflowY: 'auto' }}>
+        <div className='listing-vertical-stretch' style={{ gap: 20 }}>
+          <UiE.BasicTextField text="Quest title" font_size={24} padding_hor={10} padding_vert={5} id_="TitleInput" />
+          <div className='listing-vertical-stretch' style={{ gap: 10 }}>
+            <UiE.IconTextButtonOutline text="Complete" icon="check" content_alignment='center' id_="CompleteQBtn" onClick={NotImpl}/>
+            <UiE.IconTextButtonOutline text="Add Subquest" icon="add" content_alignment='center' id_="AddSubQBtn" onClick={openNewQPopup}/>
+            <UiE.IconTextButtonOutline text="Delete" icon="delete" content_alignment='center' id_="DeleteQBtn" onClick={NotImpl}/>
+          </div>
+          <div className='labled-text-field'>
+            <UiE.BasicLable text="Description" id_="DescriptionInput" />
+            <UiE.LongTextField text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum." />
+          </div>
+          <div className='labled-text-field'>
+            <UiE.BasicLable text="Owner" />
+            <UiE.BasicTextField text="You" id_="OwnerInput" />
+          </div>
+          <div className='labled-text-field'>
+            <UiE.BasicLable text="Editor" />
+            <UiE.BasicTextField text="User1209" id_="EditorInput" />
+          </div>
+        </div>
+        <div className='labled-text-field'>
+          <UiE.BasicLable text="Changes" />
+          <div style={{ alignSelf: 'stretch', justifyContent: 'flex-start', alignItems: 'center', gap: 20, display: 'inline-flex' }}>
+            <UiE.BasicTextButton text="Discard" id_="DiscardBtn" onClick={NotImpl}/>
+            <UiE.BasicTextButton text="Save" id_="SaveBtn" onClick={NotImpl}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
+
+function QuestList() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const handleSelect = (id) => {
+    console.log(`Quest ${id}: selected`);
+    setSelectedId(id);
+  };
+
+  const dateToQuestItem = (quest) => {
+    return (
+      <UiE.QuestItem
+        key={quest.id}
+        id_={quest.id}
+        // parent_quest={quest.parent_quest}
+        {...(quest.parent_quest && { parent_quest: quest.parent_quest })}
+        title={quest.title}
+        status={quest.status}
+        is_selected={quest.id === selectedId}
+        onClick={() => handleSelect(quest.id)}
+      >{quest.children && Array.isArray(quest.children) && quest.children.map(child => dateToQuestItem(child))}
+      </UiE.QuestItem>
+    );
+  }
+
+  const quests = [
+    {
+      id: 1, title: "Quest 1", status: "initial", children: [{
+        id: 2, title: "Quest 2", status: "done", parent_quest: 1, children: [{
+          id: 3, title: "Quest 3", status: "done", parent_quest: 2, children: [{
+            id: 7, title: "Quest 7", status: "done", parent_quest: 3
+          }]
+        }]
+      }, {
+        id: 4, title: "Quest 4", status: "initial", parent_quest: 1
+      },],
+    }, {
+      id: 5, title: "Quest 5", status: "initial", children: [{
+        id: 6, title: "Quest 6", status: "done", parent_quest: 5
+      },],
+    },
+  ];
+
+  return (
+    <div className='quest-list'>
+      {quests.map((quest) => dateToQuestItem(quest))}
+    </div>
+  );
+}
+
+function NotImpl() {
+  alert("This feature is not implemented yet!");
+  return null;
 }
 
 export default App;
---
+++ src/UiElements.js
@@ -0,0 +1,160 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+export const QuestItem = ({ title, status = 'initial', is_selected = false, onClick, children, parent_quest, id_ }) => {
+    const has_children = children ? true : false;
+    const indent_witdh = 46;
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const toggleChildren = () => {
+        console.log(`toggle: ${isExpanded}`);
+        setIsExpanded(prev => !prev);
+    };
+
+    return (
+        <div className='quest-list'>
+            <div style={{ width: '100%', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 0, display: 'flex', boxSizing: 'border-box' }}
+                {...(id_ ? { id: id_ } : {})}>
+                {parent_quest && <MaterialIcon icon='subdirectory_arrow_right' style={{ paddingLeft: `${(parent_quest && indent_witdh / 2)}px`, }} />}
+                <div className='quest-item' style={{ background: is_selected === true && 'var(--light-color)' }} onClick={onClick} >
+                    {has_children && <div id="icon_btn" onClick={toggleChildren}>
+                        <MaterialIcon icon={'arrow_drop_down'} style={{ color: is_selected === true && 'var(--dark-color)' }} />
+                    </div>}
+                    <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex' }}>
+                        <MaterialIcon
+                            icon={status === 'initial' ? "circle" : (status === 'done' ? "check_circle" : "block")}
+                            style={{ color: status == 'done' ? 'var(--completed-color)' : (is_selected === true && 'var(--dark-color)') }} />
+                        <div className='font-base' style={{ color: is_selected === true && 'var(--dark-color)' }}>{title}</div>
+                    </div>
+                </div>
+            </div>
+            {has_children && isExpanded && (
+                <div className='quest-list' style={{ paddingLeft: `${(parent_quest && indent_witdh - 5)}px`, }} >
+                    {children}
+                </div>)
+            }
+        </div>
+    )
+}
+
+export const SidebarOption = ({ text, icon = 'add', id_, onClick }) => {
+    return (
+        <div className='sidebar-option' onClick={onClick} {...(id_ ? { id: id_ } : {})}>
+            {MaterialIcon({ icon })}
+            <div className="font-heading-3">{text}</div>
+        </div>
+    )
+}
+
+export const BasicTextField = ({ text, font_size = 14, padding_hor = 10, padding_vert = 10, id_ }) => {
+    return (
+        <div className="text-field" style={{ paddingLeft: padding_hor, paddingRight: padding_hor, paddingTop: padding_vert, paddingBottom: padding_vert }}>
+            <input type="text" className="font-base input" defaultValue={text} aria-multiline={true} style={{ fontSize: font_size }} {...(id_ ? { id: id_ } : {})} />
+        </div>
+    )
+}
+
+export const LongTextField = ({ text, font_size = 14, padding_hor = 10, padding_vert = 10, id_ }) => {
+    const textareaRef = useRef(null);
+
+    const adjustHeight = () => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.style.height = 'auto';
+            textarea.style.height = `${textarea.scrollHeight}px`;
+        }
+    };
+
+    useEffect(() => {
+        adjustHeight();
+    }, [text]);
+
+    return (
+        <div className="text-field" style={{ paddingLeft: padding_hor, paddingRight: padding_hor, paddingTop: padding_vert, paddingBottom: padding_vert, }} >
+            <textarea ref={textareaRef} className="font-base input"  onInput={adjustHeight}
+                style={{ fontSize: font_size, width: '100%', resize: 'none', overflow: 'hidden', minHeight: '40px', }}
+                {...(id_ ? { id: id_ } : {})}
+            >{text}</textarea>
+        </div>
+    );
+};
+
+function auto_height(elem) {
+    elem.style.height = '1px';
+    elem.style.height = `${elem.scrollHeight}px`;
+}
+
+export const BasicLable = ({ text }) => {
+    return (
+        <div className="font-base">{text}:</div>
+    )
+}
+
+export const IconTextButtonFill = ({ text, icon = 'add', onClick, content_alignment = 'flex-start', id_ }) => {
+    return (
+        <div className="button-icon-text-fill" style={{ justifyContent: content_alignment }} onClick={onClick} {...(id_ ? { id: id_ } : {})}>
+            {MaterialIcon({ icon })}
+            <div className="font-base">{text}</div>
+        </div>
+    )
+}
+
+export const IconTextButtonOutline = ({ text, icon = 'add', onClick, content_alignment = 'flex-start', id_ }) => {
+    return (
+        <div className="button-icon-text-outline" style={{ justifyContent: content_alignment }} onClick={onClick} {...(id_ ? { id: id_ } : {})}>
+            {MaterialIcon({ icon })}
+            <div className="font-base">{text}</div>
+        </div>
+    )
+}
+
+export const BasicTextButton = ({ text, content_alignment = 'center', onClick, id_ }) => {
+    return (
+        <div className="text-button" style={{ aligment: content_alignment }} onClick={onClick} {...(id_ ? { id: id_ } : {})}>
+            <div className="font-base">{text}</div>
+        </div>
+    )
+}
+
+export const MaterialIcon = ({ icon, style = {} }) => {
+    return (
+        <span className="material-symbols-rounded" style={style}>{icon}</span>
+    )
+}
+
+export const SimpleDivider = () => {
+    return (
+        <div className="divider" />
+    )
+}
+
+export const NewQuestPopup = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+    return (
+        <div className="popup-background" onClick={onClose}>
+            <div className="listing-vertical popup-content" onClick={(e) => e.stopPropagation()}>
+                <div className='font-heading-2' style={{ textAlign: 'left' }}>New Quest</div>
+                <div className='labled-text-field'>
+                    <BasicLable text="Title" id_="DescriptionInput" />
+                    <BasicTextField id_="NewQPopTitleIn" />
+                </div>
+                <div className='labled-text-field'>
+                    <BasicLable text="Description" id_="DescriptionInput" />
+                    <LongTextField id_="NewQPopDescIn" />
+                </div>
+                <div className='labled-text-field'>
+                    <BasicLable text="Owner" id_="DescriptionInput" />
+                    <BasicTextField id_="NewQPopOwnerIn" />
+                </div>
+                <div className='labled-text-field'>
+                    <BasicLable text="Editor" id_="DescriptionInput" />
+                    <BasicTextField id_="NewQPopEditorIn" />
+                </div>
+                <div style={{ paddingTop: 30, alignSelf: 'stretch', justifyContent: 'flex-start', alignItems: 'center', gap: 20, display: 'inline-flex' }}>
+                    <BasicTextButton text="Cancel" id_="NewQPopCancelBtn" />
+                    <BasicTextButton text="Create" id_="NewQPopCreatelBtn" />
+                </div>
+            </div>
+        </div>
+    );
+};
